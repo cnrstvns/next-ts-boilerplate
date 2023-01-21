a7bac7b20d65665e36a5a685781192c50763db38
Connor Stevens <conmail1224@gmail.com>
2023-01-21 11:54:47 -0500

    Initial commit

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,7 @@
+export default function Demo() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-blue-300">
+      <div className="text-2xl font-medium">Some text should go here</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -18,31 +17,13 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By Vercel
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+        <div className={styles.thirteen}>13</div>
       </div>
 
       <div className={styles.grid}>
@@ -87,5 +68,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
